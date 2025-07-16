d7c809ff2f071e2accdf4c18a787967776b1d97e
Stefan Mehner <smehner@figma.com>
2025-07-16 17:57:19 +0200

    clean repo

--- src/ui/icons/IconMinus.tsx
+++
@@ -1,4 +0,0 @@
-import { IconProps, Icon } from "primitives";
-export const IconMinus = (props: IconProps) => (
-  <Icon {...props}><path d="M3.33337 8H12.6667" stroke="var(--svg-stroke-color)" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/></Icon>
-);
--- src/ui/images/index.ts
+++
@@ -1,2 +0,0 @@
-import placeholderSvg from "./placeholder.svg";
-export const placeholder = placeholderSvg;
